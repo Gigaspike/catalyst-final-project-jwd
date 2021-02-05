9ddee80249c475aef09f82c97cf570c912b698ac
Gigaspike <inoshishi101@gmail.com>
2021-02-05 12:58:01 -0800

    picture and text title centered

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,17 +1,15 @@
-const name = document.getElementById('taskName');
+const newTaskNameInput = document.getElementById('taskName');
 const named = document.getElementById('taskAsign');
 const details = document.getElementById('taskDetail');
 const taskDate = document.getElementById('taskDate');
 const status = document.getElementById('status');
 const form = document.getElementById('form');
 const errorElement = document.getElementById('error')
-
-
-
-
-
-
-
+//variables being applied
+const name1 = newTaskNameInput.nodeValue;
+//test area
+console.log('name: ' + name1);
+//class setup for input fields
 class Task{
     constructor(a,b,c,d,){
         this.taskAsign = 'Ex Lisa';
@@ -34,5 +32,13 @@ class Task{
     }
 }
 
+
+form.addEventListener('submit', (e) => {
+    
+})
+
+function validFormFieldInput(data){
+
+}
 
 
