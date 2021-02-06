8b14a40a17b0e9ab3d8c02ed3d0882b7d6a0412a
Gigaspike <inoshishi101@gmail.com>
2021-02-05 17:01:08 -0800

    trying to get error working with if statements

--- Scripts/Node.js
+++ Scripts/Node.js
@@ -18,3 +18,111 @@ class Node {
 }
 
 module.export = Node;
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+if(name1.value === null || name1.value === ''){
+  errorElement.style.display = 'block';
+  errorElement.innerText='Submit failed! label the task with a name is required.';
+  setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-warning');
+      errorElement.style.color = 'Red';
+      //your code to be executed after 1 second
+    }, 500); 
+    setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-danger');
+      errorElement.style.color = 'Black';
+      //your code to be executed after 1 second
+    }, 1000);
+    setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-warning');
+      errorElement.style.color = 'Red';
+      //your code to be executed after 1 second
+    }, 1500); 
+    setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-danger');
+      errorElement.style.color = 'Black';
+      //your code to be executed after 1 second
+    }, 2000); 
+}else if(asigned.value === null || asigned.value === ''){
+  errorElement.style.display = 'block';
+  errorElement.innerText='Submit failed! asigned field cannot be blank.';
+  setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-warning');
+      errorElement.style.color = 'Red';
+      //your code to be executed after 1 second
+    }, 500); 
+    setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-danger');
+      errorElement.style.color = 'Black';
+      //your code to be executed after 1 second
+    }, 1000);
+    setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-warning');
+      errorElement.style.color = 'Red';
+      //your code to be executed after 1 second
+    }, 1500); 
+    setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-danger');
+      errorElement.style.color = 'Black';
+      //your code to be executed after 1 second
+    }, 2000); 
+
+}else if(detail.value === '' || detail.value === null){
+  errorElement.style.display = 'block';
+  errorElement.innerText='Submit failed! Please fill in detailed task description.';
+  setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-warning');
+      errorElement.style.color = 'Red';
+      //your code to be executed after 1 second
+    }, 500); 
+    setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-danger');
+      errorElement.style.color = 'Black';
+      //your code to be executed after 1 second
+    }, 1000);
+    setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-warning');
+      errorElement.style.color = 'Red';
+      //your code to be executed after 1 second
+    }, 1500); 
+    setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-danger');
+      errorElement.style.color = 'Black';
+      //your code to be executed after 1 second
+    }, 2000); 
+
+}else if(setDate.value === null || setDate.value === ''){
+  errorElement.style.display = 'block';
+  errorElement.innerText='Submit failed! Due date required for each task.';
+  setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-warning');
+      errorElement.style.color = 'Red';
+      //your code to be executed after 1 second
+    }, 500); 
+    setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-danger');
+      errorElement.style.color = 'Black';
+      //your code to be executed after 1 second
+    }, 1000);
+    setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-warning');
+      errorElement.style.color = 'Red';
+      //your code to be executed after 1 second
+    }, 1500); 
+    setTimeout(function() {
+      errorElement.setAttribute('class', 'alert alert-danger');
+      errorElement.style.color = 'Black';
+      //your code to be executed after 1 second
+    }, 2000); 
+}*/
--- Scripts/main.js
+++ Scripts/main.js
@@ -1,41 +1,87 @@
 const newTaskNameInput = document.getElementById('taskName');
-const named = document.getElementById('taskAsign');
-const details = document.getElementById('taskDetail');
-const taskDate = document.getElementById('taskDate');
-const status = document.getElementById('status');
-const form = document.getElementById('form');
-const errorElement = document.getElementById('error')
+const newTaskAsigned = document.getElementById('taskAsign');
+const newTaskDetails = document.getElementById('taskDetail');
+const newTaskDate = document.getElementById('taskDate');
+//const newTaskStatus = document.getElementById('status');
+const newForm = document.getElementById('zform');
+const errorElement = document.getElementById('error');
 //variables being applied
-const name1 = newTaskNameInput.nodeValue;
+const name1 = newTaskNameInput.value;
+const asigned = newTaskAsigned.value;
+const detail = newTaskDetails.value;
+const setDate = newTaskDate.value;
+//const setstatus = newTaskStatus.value;
 //test area
 console.log('name: ' + name1);
 //class setup for input fields
-class Task{
-    constructor(a,b,c,d,){
-        this.taskAsign = 'Ex Lisa';
-        this.taskName = 'ex taskName'; 
-        this.taskDetail = 'ex  Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
-        this.taskDate = '01/02/2021';
-        this.status = 'assigned';
-    }
-    getstatus(){
-        return this.status;
-    }
-    setstatus(){
-        if (this.status === 'assigned'){
-            this.status = 'In progress';
-        }else if(this.status === 'In progress'){
-            this.status = 'Complete';
-        }else{
-            this.status = 'assigned';
-        }
-    }
-}
+// class Task{
+//     constructor(a,b,c,d,e){
+//         this.taskAsign = 'Ex Lisa';
+//         this.taskName = 'ex taskName'; 
+//         this.taskDetail = 'ex  Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
+//         this.taskDate = '01/02/2021';
+//         this.status = 'assigned';
+//     }
+//     getstatus(){
+//         return this.status;
+//     }
+//     setstatus(){
+//         if (this.status === 'assigned'){
+//             this.status = 'In progress';
+//         }else if(this.status === 'In progress'){
+//             this.status = 'Complete';
+//         }else{
+//             this.status = 'assigned';
+//         }
+//     }
+// }
 
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+newForm.addEventListener('submit', (e) => {
+    let messages = [];
+    if(name1 === null|| name1 === ''){
+        messages.push('A name for the task is required.')
+    }
+     if(asigned.value === null|| asigned.value === ''){
+        messages.push('Assign a someone to the task.')
+    }
+     if(detail.value === null|| detail.value === ''){
+        messages.push('Please leave a detail description of the task.')
+    }
+     if(setDate.value === null|| setDate.value === ''){
+        messages.push('Please select a due date.')
+    }
 
+    if(messages.length > 0){
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ');
+        errorElement.style.display = 'block';
+        setTimeout(function() {
+            errorElement.setAttribute('class', 'alert alert-warning');
+            errorElement.style.color = 'Red';
+            //your code to be executed after 1 second
+          }, 500); 
+          setTimeout(function() {
+            errorElement.setAttribute('class', 'alert alert-danger');
+            errorElement.style.color = 'Black';
+            //your code to be executed after 1 second
+          }, 1000);
+          setTimeout(function() {
+            errorElement.setAttribute('class', 'alert alert-warning');
+            errorElement.style.color = 'Red';
+            //your code to be executed after 1 second
+          }, 1500); 
+          setTimeout(function() {
+            errorElement.setAttribute('class', 'alert alert-danger');
+            errorElement.style.color = 'Black';
+            //your code to be executed after 1 second
+          }, 2000);
+    }else{
+        console.log('no errors')
+        errorElement.style.display = 'none';
+        e.preventDefault();
+    }
+    
 })
 
 function validFormFieldInput(data){
