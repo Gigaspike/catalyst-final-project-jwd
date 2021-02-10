d11cbc1194e8de9f5eaf0373790f0ac6245165ef
Gigaspike <inoshishi101@gmail.com>
2021-02-09 17:36:56 -0800

    start of task 6

--- Scripts/taskManager.js
+++ Scripts/taskManager.js
@@ -1,3 +1,17 @@
+const createTaskHtml = (insertname, insertdescription, insertassignedto, insertdate, insertstatus) => {
+    <li class="list-group-item">
+    <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
+        <h5>${insertname}</h5>  <p>${insertstatus}</p>
+        <span class="badge badge-danger">${insertdescription}</span>
+    </div>
+    <div class="d-flex w-100 mb-3 justify-content-between">
+        <small>${insertassignedto}</small>
+        <small>${insertdate}</small>
+    </div>
+    <p>Take out the trash to the front of the house</p>
+    </li>
+}
+
 class TaskManager{
     constructor(currentID = 0,){
         this.tasks = [];
